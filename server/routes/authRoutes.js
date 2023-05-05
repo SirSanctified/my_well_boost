0af25e3269437be206075d5c6a931ebb72a26caf
Pritchard Mambambo <mambambopritchard@gmail.com>
2023-05-05 13:34:09 +0200

    Update authRoutes.js to include password reset controllers

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -3,6 +3,7 @@ import {registerUserController, activateUserController} from '../controllers/reg
 import handleLogout from '../controllers/logoutUserController'
 import loginUserController from '../controllers/loginUserController'
 import handleRefreshToken from '../controllers/refreshTokenController'
+import { forgotPassword, resetPassword } from '../controllers/passwordResetController.js'
 
 const authRouter = express.Router()
 
@@ -12,5 +13,7 @@ authRouter
   .get('/logout', handleLogout)
   .post('/login', loginUserController)
   .get('/refresh-token', handleRefreshToken)
+  .post('/forgot-password', forgotPassword)
+  .post('/reset-password:userId/:token', resetPassword)
 
 export default authRouter
